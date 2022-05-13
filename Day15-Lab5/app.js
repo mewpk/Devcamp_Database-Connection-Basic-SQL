d52073d429c00bb7aeed782f4bb00c8b786b327d
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-13 10:52:10 +0700

    save

---
+++ Day15-Lab5/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+app.get('/api/users', (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root', // <== ระบุใหถูกตอง
+        password: '1234', // <== ระบุใหถูกตอง
+        database: 'codecamp',
+    });
+    // เปด connection ไปที่ database
+    connection.connect();
+    connection.query('SELECT * from user', (err, rows, fields) => {
+        if (err) throw err;
+        // return response กลับไปหา client โดยแปลง record เปน json array
+        res.json(rows);
+    });
+    // ปด connection
+    connection.end();
+});
+app.listen(3000, () => {
+    console.log('server started on port 3000!');
+});
